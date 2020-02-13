40d479418a303ed87ad20d08d6906b6d8674998e
tw-tjd <tj.daw@thermoworks.com>
2020-02-13 14:54:54 -0700

    material  imports updated

--- projects/ngx-mat-intl-tel-input-tester/src/app/app.module.ts
+++ projects/ngx-mat-intl-tel-input-tester/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import {NgxMatIntlTelInputModule} from '../../../ngx-mat-intl-tel-input/src/lib/ngx-mat-intl-tel-input.module';
-import {MatButtonModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import { NgxMatIntlTelInputModule } from '../../../ngx-mat-intl-tel-input/src/lib/ngx-mat-intl-tel-input.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
